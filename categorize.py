60d647016b561a003dae627b3554d447b2b911c1
Gleiphir <zhangzx.1997@163.com>
2021-11-01 14:41:37 +0900

    Update categorize.py

--- categorize.py
+++ categorize.py
@@ -84,6 +84,7 @@ if __name__ =='__main__':
 				print(predict.cpu().detach()[target])
 				print(predict.cpu().detach()[:,target])
 				print(predict.cpu().detach()[target,:])
+				print(torch.index_select(predict, 0, target))
 				print(torch.index_select(predict,1,target) )
 				#print(torch.argmax(predict,dim=1))
 				#print(confid)
