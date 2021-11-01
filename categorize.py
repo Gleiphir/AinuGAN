0f0267d24f45120cd9ccc8b0bac818218c99d67c
Gleiphir <zhangzx.1997@163.com>
2021-11-01 14:10:20 +0900

    Update categorize.py

--- categorize.py
+++ categorize.py
@@ -77,6 +77,7 @@ if __name__ =='__main__':
 				now_t = time.time()
 
 				print("##############################")
+				print(target)
 				print(predict.size())
 				print(predict[target].size())
 				print('\n')
