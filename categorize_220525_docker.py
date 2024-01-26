c3e769dab47b71bd5c2e8c9fbb4e83708abf188c
Gleiphir <zhangzx.1997@163.com>
2024-01-26 11:11:31 +0900

    220525

---
+++ categorize_220525_docker.py
@@ -0,0 +1,98 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+#from torch.optim.lr_scheduler import ExponentialLR
+from torchvision import datasets, transforms
+from torch.autograd import Variable
+import torchvision
+from model_category_220523 import Classifier
+import time
+import os
+
+BATCH_SIZE = 8
+
+dataset_path = os.environ['DATASET_PATH']
+ckpt_path = os.environ['CKPT_PATH']
+
+
+dset = datasets.ImageFolder(dataset_path, transform=transforms.Compose([
+		transforms.RandomCrop(256),
+		transforms.ToTensor(),
+		transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))]))
+
+loader = torch.utils.data.DataLoader(dset,
+	batch_size=BATCH_SIZE, shuffle=True, num_workers=8, pin_memory=True)
+
+print(dset.class_to_idx)
+
+N_CLASS = len(dset.classes)
+CL = Classifier(N_CLASS).cuda()
+print(dset.classes)
+optim = optim.Adam( CL.parameters(),
+	 lr=2e-4, betas=(0.5,0.999)
+	)
+g_iter=0
+start_t=0
+last_t=0
+now_t=0
+
+OVER_ITER = 100000
+
+over_flag = False
+
+L = Classifier(N_CLASS).cuda()
+
+
+if __name__ =='__main__':
+	start_t = time.time()
+	while not over_flag:
+		for batch_idx, (data, target) in enumerate(loader):
+			if data.size()[0] != BATCH_SIZE:
+				continue
+			data, target = Variable(data.cuda()), Variable(target.cuda())
+
+			# update discriminator
+			d_losses = []
+
+			optim.zero_grad()
+			# print(generator(z).size())
+			losses = []
+
+			predict = CL(data)
+			#print(target,data.size(1),data.size())
+			target_onehot = F.one_hot(target,num_classes=N_CLASS)
+
+			#print(predict.size(),target_onehot.size())
+			loss_fn = nn.CrossEntropyLoss()
+			loss = loss_fn(predict, target_onehot.to(torch.float)).mean()
+
+			loss.backward()
+
+			optim.step()
+
+			g_iter += 1
+
+			if batch_idx % 100 == 0:
+				last_t = now_t
+				now_t = time.time()
+
+				confid = predict[torch.arange(BATCH_SIZE),target]
+
+				print("##############################")
+
+
+				print("iter : %6d ------- time: %4d of %6d Sec" % (g_iter, now_t - last_t, now_t - start_t))
+				print('real:{},pred:{}\nconfid: {}\n \tloss: {:.6f}'.format(
+					target.tolist(),
+					torch.argmax(predict,dim=1).tolist(),
+					','.join( "{:.3f}".format(a) for a in  confid.tolist()),
+					loss.item())
+				)
+			if g_iter % 10000 == 0:
+
+				torch.save(CL.state_dict(),'{}/clsf_{}'.format(ckpt_path,g_iter))  # args.checkpoint_dir, 'gen_{}'.format(g_iter)))
+			if g_iter >= OVER_ITER:
+				over_flag = True
+				break
+
